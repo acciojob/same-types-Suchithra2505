bdfdec183d44c170dcc7a94750e54519452823aa
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-10 18:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function isSameType(value1, value2) {
   //your js code here
-	if((typeof value1) === (typeof value2)){
-		return true;
-	}
-		return false;
+	if(isNaN(value1) && isNaN(value2)) {
+    return true;
+  }
+  else if(isNaN(value1) || isNaN(value2)) {
+    return false;
+  }
+  else if(typeof value1 == typeof value2) {
+    return true;
+  } 
+  else {
+    return false;
+  }
 }
 
 // do not change the code below.
